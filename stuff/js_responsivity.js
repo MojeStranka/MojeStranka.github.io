adc26644790b4a62f42f7976b3f5df35d1122115
Jan Pánik <97601609+MojeStranka@users.noreply.github.com>
2023-04-24 16:44:40 +0200

    Update js_responsivity.js

--- stuff/js_responsivity.js
+++ stuff/js_responsivity.js
@@ -1,7 +1,4 @@
 // responzivní interface
-var functionR() {
-  var bodyEl = document.getElementById("body");
-  bodyEl.style.width = 0.9*window.innerWidth + "px";
-  bodyEl.style.height = 0.9*window.innerHeight + "px";
-};
-var responsivity = window.setInterval(functionR, 10);
+var bodyEl = document.getElementById("body");
+bodyEl.style.width = 0.9*window.innerWidth + "px";
+bodyEl.style.height = 0.9*window.innerHeight + "px";
